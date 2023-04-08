b9e681488bffb66f1960eea4ea6e58cb3e103969
Pavel Tretyakov <paveltretyakov.ru@gmail.com>
2023-04-08 23:48:00 +1000

    Updated home page

--- lib/src/darttrack.dart
+++ lib/src/darttrack.dart
@@ -1,5 +1,8 @@
 import 'controller.dart';
 
+/// Main function of the application
+///
+/// Start watch [dir] files changes and run [script] when is it happened.
 void darttrack(String dir, String script) {
   try {
     Controller(dir, script);
